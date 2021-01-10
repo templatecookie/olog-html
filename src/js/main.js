746df6d7b51bf389bbc0830dd650893c45f47fbf
Hridoy <hridoyhalderpartho@gmail.com>
2021-01-10 15:06:03 +0600

    complete coustomer review section

--- src/js/main.js
+++ src/js/main.js
@@ -18,12 +18,36 @@ $(document).ready(function () {
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    arrows: false,
-    autoplay: false,
+    autoplay: true,
     arrows: true,
     autoplaySpeed: 2000,
     prevArrow: '.features-wrapper .prev-arrow',
     nextArrow: '.features-wrapper .next-arrow',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  //Customersreview Slider
+  $('.customersreview-active').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    autoplaySpeed: 2000,
+    prevArrow: '.customersreview-wrapper .prev-arrow',
+    nextArrow: '.customersreview-wrapper .next-arrow',
     responsive: [{
         breakpoint: 1024,
         settings: {
