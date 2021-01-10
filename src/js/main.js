a624fa347e9ae1d9080f9b8bb982b3dae08f3757
Hridoy <hridoyhalderpartho@gmail.com>
2021-01-10 20:29:38 +0600

    all section align and compleate category section

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,4 @@
 $(document).ready(function () {
-  //Form input
-  var textInput = document.querySelector('input');
-  console.log(textInput)
-  console.log(textInput.nextElementSibling)
-
-  textInput.addEventListener('focusout', () => {
-    console.log(textInput.value)
-    if (textInput.value === '') {
-      textInput.nextElementSibling.classList.remove('has-value')
-    } else {
-      textInput.nextElementSibling.classList.add('has-value')
-    }
-  });
 
   //testimonial_slider
   $('.features-active').slick({
