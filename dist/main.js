6f47d9a23617c7249a9b350b2684bd9bbe1854b3
Web-Jahid <web.jahidulislam@gmail.com>
2021-01-12 09:54:50 +0600

    Dashboard Nav Added with Responsive

--- dist/main.js
+++ dist/main.js
@@ -102,9 +102,9 @@ eval("$(document).ready(function () {\n  //testimonial_slider\n  $('.features-ac
   !*** ./src/scss/main.scss ***!
   \****************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: File to import not found or unreadable: C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\src\\\\scss\\\\pages\\\\_billing.scss.\\n        on line 35 of C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\src\\\\scss\\\\main.scss\\n>> @import 'pages/billing';\\r\\n   ^\\n\\n    at C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.callback (C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:73:7)\\n    at Object.done [as callback] (C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (C:\\\\Users\\\\User\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
 
 /***/ }),
 
