3a0d4fd1742e427ee633e8eaa2043c5231c993a6
Hridoy <hridoyhalderpartho@gmail.com>
2021-01-13 15:46:25 +0600

    brand area responsive

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,37 @@ $(document).ready(function () {
     }
   });
 
+  //Brand_slider
+  $('.brand-area').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
   //testimonial_slider
   $('.features-active').slick({
     infinite: true,
