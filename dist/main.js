e5aa2ce0471618732142964c97003e3f34d0d712
Hridoy <hridoyhalderpartho@gmail.com>
2021-01-17 14:44:59 +0600

    payment input fix

--- dist/main.js
+++ dist/main.js
@@ -102,9 +102,9 @@ eval("$(document).ready(function () {\n  // Sticky Menu\n  $(window).on('scroll'
   !*** ./src/scss/main.scss ***!
   \****************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: Invalid CSS after \\\"<\\\": expected 1 selector or at-rule, was \\\"<<<<<<< HEAD\\\"\\n        on line 1 of src/scss/pages/_payment.scss\\n        from line 48 of C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\src\\\\scss\\\\main.scss\\n>> <<<<<<< HEAD\\r\\n   ^\\n\\n    at C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20\\n    at C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.callback (C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:73:7)\\n    at Object.done [as callback] (C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (C:\\\\Users\\\\PC-2\\\\Desktop\\\\olog-ecommerce-template\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/scss/main.scss?");
 
 /***/ }),
 
